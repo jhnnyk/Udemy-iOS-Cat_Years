3e45bb0a7d877990a435b031f6460240f4a94462
John Knotts <jhnnyk@gmail.com>
2016-11-06 15:39:40 -0700

    cat years functionality

--- Cat Years/ViewController.swift
+++ Cat Years/ViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ageTextField: UITextField!
+    @IBOutlet weak var ageLabel: UILabel!
+    
+    @IBAction func submitPressed(_ sender: AnyObject) {
+        
+        let catYears = Int(ageTextField.text!)! * 7
+        
+        ageLabel.text = String(catYears)
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
